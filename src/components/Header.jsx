b5aef6346e36db1d0af49b42cb2d1d0d6afe2262
Bruno Rodrigues <bruno.brds@hotmail.com>
2020-02-09 15:07:45 -0300

    feat: create basic struct

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="template-header">
+        <img
+          alt="logo"
+          src="https://freevectorlogo.net/wp-content/uploads/2012/10/pokemon-logo-vector-400x400.png"
+        />
+      </div>
+    );
+  }
+}
+
+export default Header;
